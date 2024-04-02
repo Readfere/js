16cc8fd4e11e2dfd20b0d8106be120915c73e332
Readfere <152289805+Readfere@users.noreply.github.com>
2024-04-02 09:22:11 +0800

    Update memos2.js

--- memos2.js
+++ memos2.js
@@ -27,7 +27,7 @@ function loadCssCode(code){
 const allCSS = `
 #bber{margin-top:1rem;width:auto!important;min-height:100vh;}
 .bb-timeline ul{margin:0;padding:0;}
-.bb-timeline ul li{margin-bottom:3rem;list-style-type:none!important;}
+.bb-timeline ul li{margin-bottom:3rem;list-style-type:none !important;}
 .bb-timeline ul li .bb-cont ul li{margin-bottom:0;}
 .bb-timeline .bb-item,.bb-load button{border:1px solid #dcdcdc;border-radius:8px;box-shadow:3px 3px 5px rgba(0,0,0,.1);}
 .bb-timeline .bb-item{padding:.6rem 1rem .6rem;font-size:16px;}
